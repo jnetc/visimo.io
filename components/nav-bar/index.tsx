951c73e98a292fb7892111ca47be76f16b625166
Anton <frontendevs@gmail.com>
2023-07-06 15:57:28 +0300

    fixed visik animation / added mobile navigation

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
+// Components
 import Logo from './logo';
 import Links from './links';
+import MobNavButton from './mob-menu/MobNavButton';
+import MobMenu from './mob-menu';
 import Langs from './langs';
 import Themes from './themes';
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openMenu(value: boolean) {
+    setIsOpen(value);
+    if (value) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.removeAttribute('style');
+    }
+  }
+
   return (
-    <nav className="nav-bar">
-      <Logo />
-      <Links />
-      <Langs />
-      <Themes />
-    </nav>
+    <>
+      <MobMenu handler={openMenu} isOpen={isOpen} />
+      <header aria-label="header-navigation" className="nav-bar">
+        <Logo />
+        <Links />
+        <Langs />
+        <Themes />
+        <MobNavButton handler={openMenu} />
+      </header>
+    </>
   );
 }
