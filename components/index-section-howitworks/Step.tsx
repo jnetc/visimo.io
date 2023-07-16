a148485e236a33c4c523f186a3bf0383949fb312
Anton <frontendevs@gmail.com>
2023-07-16 20:25:33 +0300

    dialog was created for steps section

--- components/index-section-howitworks/Step.tsx
+++ components/index-section-howitworks/Step.tsx
@@ -2,6 +2,7 @@ import StepBackLayer from './SVG/StepBackLayer';
 import Star from '@Components/SVG/Star';
 import SmallCloudLeft from '@Components/SVG/SmallCloudLeft';
 import Monolog from './SVG/Monolog';
+import Dialog from './SVG/Dialog';
 
 // Types
 import type { IStep } from '@Types';
@@ -24,9 +25,9 @@ export default function Step({ id, data }: Props) {
         <Star extraClass="step-star3" />
         <Star extraClass="step-star4" />
         <Star extraClass="step-star5" />
-        <p className={`step__description ${data.discuss}-number${id}`}>
+        <p className={`step__description ${data.discuss}`}>
           {data.description}
-          <Monolog />
+          {data.discuss === 'monolog' ? <Monolog /> : <Dialog />}
         </p>
       </div>
       <div className="step__app-preview">preview</div>
