d8be1d2fd4f95d8e8f9c9426735d2bf4a3789186
Anton <frontendevs@gmail.com>
2023-07-18 16:38:33 +0300

    shifted app illustration to center, bonfire animated OK

--- components/index-section-app/index.tsx
+++ components/index-section-app/index.tsx
@@ -9,20 +9,28 @@ export default function AppSection() {
   return (
     <section className="section main-grid app ">
       <div className="app__container">
-        <SmallCloudLeft extraClass="app__cloud-left" />
-        <SmallCloudRight extraClass="app__cloud-right" />
+        <SmallCloudLeft extraClass="app__cloud-left" aria-hidden />
+        <SmallCloudRight extraClass="app__cloud-right" aria-hidden />
         <img className="app__front-layer" src="/images/svg/app-front-layer.svg" alt="visics" />
-        <div className="bonfire" aria-hidden></div>
+        <div className="app__bonfire-container" aria-hidden>
+          <div className="app__bonfire">
+            <img src="/images/svg/bonfire-1.svg" alt="bonfire" className="animated-bonfire" />
+            <img src="/images/svg/bonfire-2.svg" alt="bonfire" className="animated-bonfire" />
+            <img src="/images/svg/bonfire-3.svg" alt="bonfire" className="animated-bonfire" />
+            <img src="/images/svg/bonfire-4.svg" alt="bonfire" className="animated-bonfire" />
+            <img src="/images/svg/bonfire-5.svg" alt="bonfire" className="animated-bonfire" />
+          </div>
+        </div>
         <Smartphone isApp customClass="phone-app">
           <img src="/images/screenshot.png" alt="app image" />
         </Smartphone>
-        <AppBackLayer extraClass="app__back-layer" />
-        <Star extraClass="app-star1" />
-        <Star extraClass="app-star2" />
-        <Star extraClass="app-star3" />
-        <Star extraClass="app-star4" />
-        <Star extraClass="app-star5" />
-        <Star extraClass="app-star6" />
+        <AppBackLayer extraClass="app__back-layer" aria-hidden />
+        <Star extraClass="app-star1" aria-hidden />
+        <Star extraClass="app-star2" aria-hidden />
+        <Star extraClass="app-star3" aria-hidden />
+        <Star extraClass="app-star4" aria-hidden />
+        <Star extraClass="app-star5" aria-hidden />
+        <Star extraClass="app-star6" aria-hidden />
       </div>
     </section>
   );
